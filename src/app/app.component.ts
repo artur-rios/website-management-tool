b3796ec8a4d1711f13cfc5c2c3089337845bb677
Artur Rios <arturdev@duck.com>
2023-09-09 13:33:14 -0300

    chore: removing test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public onClick(event: any) {
-    console.log('CLICK:', event);
-  }
-}
+export class AppComponent {}
